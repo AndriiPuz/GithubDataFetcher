8e8e5d5b631c80623b0ef81ee6f09c2eaaf4b424
Andrii <puzyrandrii@gmail.com>
2023-12-03 02:44:47 +0200

    add style and pagination

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -2,7 +2,7 @@ class GithubController < ApplicationController
   def search
     if params[:github_login].present?
       @user = GithubService.get_user(params[:github_login])
-      @repos = GithubService.get_user_repos(params[:github_login])
+      @repos = Kaminari.paginate_array(GithubService.get_user_repos(params[:github_login])).page(params[:page]).per(10)
     else
       flash.now[:alert] = I18n.t('github.please_enter_username')
     end
